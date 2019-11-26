27bf3fa002f302cb8781c2567f01091d089f4d67
James Tang <jytang10@gmail.com>
2019-11-25 19:45:48 -0800

    Added weather api and rendered on DOM

--- weather.js
+++ weather.js
@@ -1,4 +1,17 @@
+const weather = document.querySelector('.js-weather');
+
+const API_KEY = "";
 const COORDS = 'coords';
+
+function getWeather(lat, lng){
+  fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`)
+  .then(res => res.json())
+  .then(json => {
+    const temperature = json.main.temp;
+    const place = json.name;
+    weather.innerText = `${temperature} @ ${place}`;
+  })
+}
 
 function saveCoords(coordsObj){
   localStorage.setItem(COORDS, JSON.stringify(coordsObj));
@@ -12,6 +25,7 @@ function handleGeoSuccess(position){
     longitude
   };
   saveCoords(coordsObj);
+  getWeather(latitude, longitude);
 }
 
 function handleGeoError(){
@@ -23,11 +37,12 @@ function askForCoords(){
 }
 
 function loadCoords(){
-  const loadedCords = localStorage.getItem(COORDS);
-  if(loadedCords === null){
+  const loadedCoords = localStorage.getItem(COORDS);
+  if(loadedCoords === null){
     askForCoords();
   } else {
-
+    const parsedCoords = JSON.parse(loadedCoords);
+    getWeather(parsedCoords.latitude, parsedCoords.longitude)
   }
 }
 
